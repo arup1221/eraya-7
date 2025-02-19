ef76eb180c0328c75beb77317dcdacf07a7dab4f
ARUP GOPE <gopea999@gmail.com>
2025-02-19 16:39:44 +0530

    background updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["placehold.co"],
-//   },
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
